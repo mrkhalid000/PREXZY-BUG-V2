3a616982dc7e41af4e72500859d82239fe1a8fd7
mrkhalid000 <kingcoding11@gmail.com>
2024-09-04 11:50:49 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 require("./all/module")
 
-global.owner = "https://whatsapp.com/channel/0029VaaUfPO8qIzztuf42D04" 
-global.namabot = "𝕻𝕽𝕰𝖃𝖄 𝕭𝖀𝕾 𝕻𝟐" //BOT NAME
-global.namaCreator = "*PRECIOUS AYOMIDE*" //CREATOR NAME
+global.owner = "2348023898600" 
+global.namabot = "ᏦᏆᑎᏀ ᏦᕼᗩしᏆᗞ bug bot" //BOT NAME
+global.namaCreator = "*ᏦᏆᑎᏀ ᏦᕼᗩしᏆᗞ*" //CREATOR NAME
 global.autoJoin = false //DON'T CHANGE  / JANGAN GANTI
 global.antilink = false //DON'T CHANGE  / JANGAN GANTI
 global.versisc = '1.0.0' //DON'T CHANGE 
 global.sessionName = 'session'
 global.codeInvite = ""
-global.imageurl = 'https://telegra.ph/file/6b841282edd26fa590b6c.jpg' //GANTI PP MU MENGGUNAKAN LINK TELEGRA PH
+global.imageurl = 'https://telegra.ph/file/9c48d6c33fe541f56a2b3.jpg' //GANTI PP MU MENGGUNAKAN LINK TELEGRA PH
 global.isLink = 'https://whatsapp.com/channel/0029VaaUfPO8qIzztuf42D04' 
 global.thumb = fs.readFileSync("./thumb.png") ///DON'T CHANGE  
 global.audionya = fs.readFileSync("./all/sound.mp3") //DON'T CHANGE  
-global.packname = "𝐁𝐲 𝕻𝕽𝕰𝖃𝖄 𝕭𝖀𝕾 𝕻𝟐 " 
-global.author = "*PRECIOUS AYOMIDE👑*" 
+global.packname = "ᏦᏆᑎᏀ ᏦᕼᗩしᏆᗞ " 
+global.author = "*ᏦᏆᑎᏀ ᏦᕼᗩしᏆᗞ*" 
 global.jumlah = "5" ////DON'T CHANGE
 
 let file = require.resolve(__filename)
